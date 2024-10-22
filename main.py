708375ffc9feadfae57ccd02a3fe18986692c798
Stonekrave <scttpttn@gmail.com>
2024-10-22 13:50:38 -0500

    Added player and circleshape classes. Updated constants and main.

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
   pygame.init()
+  clock = pygame.time.Clock()
+  dt = 0
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   pygame.display.set_caption("Asteroids")
+  player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
   run = True
   while run:
-    pygame.Surface.fill(screen, ("black"))
-    pygame.display.flip()
+    
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+    player.update(dt)
+    pygame.Surface.fill(screen, ("black"))
+    player.draw(screen)
+    pygame.display.flip()
+    #Limit the framerate to 60 FPS
+    dt = (clock.tick(60)) / 1000
+    
 
-  print("Starting asteroids!")
-  print(f"Screen width: {SCREEN_WIDTH}")
-  print(f"Screen height: {SCREEN_HEIGHT}")
+    
+
+    
+
 
 
 
